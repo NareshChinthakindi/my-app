5af05961f0d82f5eb14c49351e0a2aee4d41d354
Naresh Chinthakindi <naresh_chinthakindi@epam.com>
2019-01-14 20:23:49 +0530

    Commiting the changes

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import classes from './Cockpit.css';
 
 
 const cockpit = (props) =>{
 
+    const assignedClasses = [];
+    let btnClass = '';
+
+    if(props.showPersons) {
+        btnClass = classes.Red;
+    }
+
+
+    if (this.state.persons.length <=2) {
+        assignedClasses.push(classes.red);
+    }
+
+    if (this.state.persons.length <=1) {
+        assignedClasses.push(classes.bold);
+    }
+
     return (
-        <div>
-    <h1>Hi Iam react</h1>
-    <p className={classes.join(' ')}>This is really working </p>
-    <button onClick={this.toogleEventHandler} style={style}>Toogle Names</button>
+        <div className={classes.Cockpit}>
+            <h1>Hi Iam react</h1>
+            <p className={assignedClasses.join(' ')}>This is really working </p>
+            <button onClick={this.toogleEventHandler} style={style}>Toogle Names</button>
     </div>
     );
 }
+
+export default cockpit;
